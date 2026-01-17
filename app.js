f82a104daebc4d550f83c611603275f7b9404abe
Yash gaikwad <gaikwadyash964@gmail.com>
2026-01-17 14:18:11 +0530

    aaaa

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from EC2! Your pipeline is working. all done final step');
+  res.send('Hello from EC2! Your pipeline is working. all done final step jenkins');
 });
 
 // Export for testing, but only listen if not in a test environment
